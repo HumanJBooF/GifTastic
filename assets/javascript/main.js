9ad2ec41c1ef2e1744a9c51ec66aed399c8871f9
HumanJBooF <humanjboof@gmail.com>
2018-07-17 13:44:23 -0400

    console log response working

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -1,3 +1,23 @@
 $(function() {
-	
-})();
+    
+    var moviesTvGames = [  'A Clockwork Orange',
+                           'American Psycho',
+                           'Fargo',
+                           'Nier Automata',
+                           'Street Fighter',
+                           'Metal Gear Solid',
+                           'The Legend of Zelda',
+                           'Twin Peaks',
+                           'Dexter',
+                           'Dark Souls'];
+
+    var queryURL =        "https://api.giphy.com/v1/gifs/search?q=" +moviesTvGames[0] + "&api_key=21pbXpSdx68vgJpuoB7wb0uQgVGGuGUg";
+
+   $.ajax({
+       url: queryURL,
+       Method: "GET"
+   }).then(function(response){
+       console.log(response)
+   })
+
+});
